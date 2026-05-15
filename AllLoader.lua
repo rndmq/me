e32cefeb8d29b2feb953eb9edcf98392c39df436
rndmq <kevinsultanto8@gmail.com>
2026-05-15 16:35:45 +0700

    n

---
+++ AllLoader.lua
@@ -0,0 +1,42 @@
+local a = {
+    ["Dig To Escape 1"] = {
+        73510530738011,
+        92122513197996
+    },
+}
+
+local b = nil
+
+for c, d in pairs(a) do
+    for _, e in ipairs(d) do
+
+        if game.GameId == e then
+            b = c
+            break
+        end
+    end
+
+    if b then
+        break
+    end
+end
+
+if b then
+
+    local f = b:match("(%d+)$")
+
+    if f then
+        local g =
+            "https://raw.githubusercontent.com/rndmq/me/refs/heads/main/game/Game"
+            .. f
+
+        loadstring(game:HttpGet(g))()
+
+        print("Loaded:", b)
+    else
+        warn("No number found")
+    end
+
+else
+    warn("Unsupported Game")
+end
